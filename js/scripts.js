528f1dd14d5c89638f205c7122bfd79553b3fdb0
Maria Lujan <mariaalexandralujan@gmail.com>
2024-05-15 14:39:36 -0300

    agrego js para el header y footer en las distintas seciones

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Cargar el header desde index.html
+    fetch('../index.html')
+        .then(response => response.text())
+        .then(data => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(data, 'text/html');
+            const header = doc.getElementById('header').innerHTML;
+            document.getElementById('header-placeholder').innerHTML = header;
+
+            const footer = doc.getElementById('footer').innerHTML;
+            document.getElementById('footer-placeholder').innerHTML = footer;
+        });
+});
