431c1d78f39b382b8f213ff56aaff1c29e7724ec
Maria Lujan <mariaalexandralujan@gmail.com>
2024-05-18 16:58:35 -0300

    prueboi otra alternativa js

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,6 @@ function includeHTML() {
                 .then(data => {
                     el.innerHTML = data;
                     el.removeAttribute('data-include');
-                    adjustPaths(el); // Ajusto las rutas relativas
                     includeHTML(); // Llama recursivamente para incluir elementos anidados
                 })
                 .catch(err => console.error('Error including HTML:', err));
@@ -16,26 +15,7 @@ function includeHTML() {
     });
 }
 
-function adjustPaths(container) {
-    const links = container.querySelectorAll('a[data-root]');
-    links.forEach(link => {
-        const href = link.getAttribute('href');
-        const depth = getRelativeDepth();
-        const newHref = depth + href;
-        link.setAttribute('href', newHref);
-    });
-}
+document.addEventListener('DOMContentLoaded', includeHTML);
 
-function getRelativeDepth() {
-    const path = window.location.pathname.split('/');
-    const repository = 'https://github.com/MaLujan/grupo17_comision24265'; 
-    const index = path.indexOf(repository);
-    const depth = path.length - index - 3; 
-    let relativePath = '';
-    for (let i = 0; i < depth; i++) {
-        relativePath += '../';
-    }
-    return relativePath;
-}
 
 document.addEventListener('DOMContentLoaded', includeHTML);
